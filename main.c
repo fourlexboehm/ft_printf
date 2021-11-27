1d64a0f98ba52b142c1f70ae1e5075e5d3c3218e
Alex Boehm <alexb@ozrunways.com>
2021-11-27 20:28:48 +1030

    Is it done

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "includes/ft_printf.h"
+#include <stdio.h>
+
 int main ()
 {
 //	ft_printf("test\n");
@@ -7,6 +9,10 @@ int main ()
 	ft_printf("\ntest unsigned int: %u", 28300033);
 	ft_printf("\ntest string: %s", "I'm a string");
 
-	ft_printf("\n\nMultitest Char: %c\n Int: %i\n String: %s", 'y', 42, "I'm also a string");
+	int numb = 5;
+	printf("\n%p\n", &numb);
+	ft_printf("\n%x\n", 8293482);
+	ft_printf("\n%X\n", 8293482);
+//	ft_printf("\n\nMultitest Char: %c\n Int: %i\n String: %s", 'y', 42, "I'm also a string");
 	ft_printf("\n");
 }
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aboehm <aboehm@42adel.org.au>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 21:42:11 by aboehm            #+#    #+#             */
-/*   Updated: 2021/11/24 21:55:49 by aboehm           ###   ########.fr       */
+/*   Updated: 2021/11/27 20:18:12 by aboehm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,35 @@ int	write_str(char *str)
 	return (ft_strlen(str));
 }
 
+int put_hex(size_t n, char base, size_t count)
+{
+	char *basep;
+
+		if (base == 'x')
+			basep = "0123456789abcdef";
+		else
+			basep = "0123456789ABCDEF";
+	if (n >= 16)
+		put_hex(n / 16, base, count);
+	ft_putchar_fd(basep[n % 16], 1);
+	count++;
+	return (count);
+}
+
+int	put_pointer(void *pointer)
+{
+	size_t count;
+	
+	count = 0;
+	ft_putstr_fd("0x", 1);
+	return (2 + (put_hex((size_t)pointer, 'x', count)));
+}
+
 int	flag_select(char *str_in, va_list arglist, size_t count)
 {
 	if (*str_in == 'd' || *str_in == 'i')
 	{
-		ft_putnbr_fd((int)va_arg(arglist, int), 1);
+		ft_putnbr_fd((long)va_arg(arglist, int), 1);
 		count += sizeof(int);
 	}
 	else if (*str_in == 'c')
@@ -32,15 +56,15 @@ int	flag_select(char *str_in, va_list arglist, size_t count)
 	}
 	else if (*str_in == 's')
 		count += write_str(va_arg(arglist, char *));
-//	else if (*str_in == 'p')
-//		count += put_pointer();
+	else if (*str_in == 'p')
+		count += put_pointer(va_arg(arglist, void *));
 	else if (*str_in == 'u')
 	{
 		ft_putnbr_fd(va_arg(arglist, int), 1);
 		count += sizeof(int);
 	}
-//	else if (*str_in == 'x'|| *str_in == 'X')
-//		
+	else if (*str_in == 'x'|| *str_in == 'X')
+		count = put_hex(va_arg(arglist, int), *str_in, count);
 	return (count);
 }
 
